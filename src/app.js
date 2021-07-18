e0efa49c8e959ab83dc183e2e867a836a7340e6d
mdiedricks <michael.diedricks@gmail.com>
2021-07-18 11:35:50 +1000

    cors enabled

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 require("./db/mongoose");
+const cors = require("cors");
 const usersRouter = require("./routes/user");
 // const plantsRouter = require("./routes/plant");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(usersRouter);
 // app.use(plantsRouter);
